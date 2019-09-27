979a6ea95e6c76637cd0c1ee78fe6ae8e278ae96
András Ács <5522871+andracs@users.noreply.github.com>
2019-09-27 14:03:50 +0200

    Update Main.java

--- src/Opgave1/Main.java
+++ src/Opgave1/Main.java
@@ -4,19 +4,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Instatierer et scannerobjekt, som han senere vil bruge til at læse værdier ind med
         Scanner input = new Scanner(System.in);
+
+        // Genererer et tilfældigt tal mellem 0..9-10 stykker
         int tal = (int)(Math.random()*10);
+
+        // Gæt er -1 så den hopper ind i lykken
         int gaet = -1;
+
+        // Løkke, der kører så længe, indtil nummeret er gættet
         while (gaet != tal) {
-            gaet = input.nextInt();
+            gaet = input.nextInt(); // Tal indlæses med scanner
             if (gaet < tal) {
                 System.out.println("for lavt");
             } else if (gaet > tal){
                 System.out.println("for højt");
             }
         }
+
+        // Hvis tal er fundet, udskrives følgende
         System.out.println("Computerens tal = " + tal + " derfor gættede du rigtigt");
-
-
+        
     }
 }
